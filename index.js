1b7105175089b2565f40583816b4805c3bc8bf0d
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-07-07 15:44:53 +0530

    Reducer -Pure function in Switch Statement

--- index.js
+++ index.js
@@ -13,13 +13,22 @@ const buyLaptop = () => {
 }
 
 // Reducer -Pure function
-const reducer = (state = initialState, action){
-
+const reducer = (state = initialState, action) => {
     // Switch Statement
     switch (action.type) {
         case 'BUY_LAPTOP':
-            return {numOflaptops: numOflaptops-1}    
+            return { numOflaptops: state.numOflaptops - 1 }
         default:
             return state;
     }
 }
+console.clear();
+// createStore
+const store = createStore(reducer);
+// console.log(store);
+store.subscribe(() => { console.log(store.getState()) });
+store.dispatch(buyLaptop()); // { numOflaptops: 99 }
+store.dispatch(buyLaptop()); // { numOflaptops: 98 }
+store.dispatch(buyLaptop()); // { numOflaptops: 97 }
+store.dispatch(buyLaptop()); // { numOflaptops: 96 }
+store.dispatch(buyLaptop()); // { numOflaptops: 95 }
